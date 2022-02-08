f1fe5fa2c4b90b81cc39ff07b5488d2369241671
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-02-08 14:09:30 +0100

    Initial version of Gateway generated by generator-jhipster@7.5.0 with blueprints: generator-jhipster-kotlin@1.14.0

---
+++ kateway/src/test/kotlin/backend/gateway/config/NoOpMailConfiguration.kt
@@ -0,0 +1,21 @@
+package backend.gateway.config
+
+import backend.gateway.domain.User
+import backend.gateway.service.MailService
+import org.mockito.Mockito
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class NoOpMailConfiguration {
+    private val mockMailService = Mockito.mock(MailService::class.java)
+
+    @Bean
+    fun mailService(): MailService {
+        return mockMailService
+    }
+
+    init {
+        Mockito.doNothing().`when`(mockMailService).sendActivationEmail(User())
+    }
+}
---
+++ kateway/src/test/kotlin/backend/gateway/web/rest/PublicUserResourceIT.kt
@@ -0,0 +1,85 @@
+package backend.gateway.web.rest
+
+import backend.gateway.IntegrationTest
+import backend.gateway.domain.User
+import backend.gateway.repository.UserRepository
+import backend.gateway.security.ADMIN
+import backend.gateway.security.USER
+import backend.gateway.service.EntityManager
+import backend.gateway.service.dto.UserDTO
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.web.reactive.server.WebTestClient
+
+/**
+ * Integration tests for the {@link UserResource} REST controller.
+ */
+@AutoConfigureWebTestClient
+@WithMockUser(authorities = [ADMIN])
+@IntegrationTest
+class PublicUserResourceIT {
+
+    private val DEFAULT_LOGIN = "johndoe"
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @Autowired
+    private lateinit var em: EntityManager
+
+    @Autowired
+    private lateinit var webTestClient: WebTestClient
+
+    private lateinit var user: User
+
+    @BeforeEach
+    fun initTest() {
+        user = UserResourceIT.initTestUser(userRepository, em)
+    }
+
+    @Test
+    fun getAllPublicUsers() {
+        // Initialize the database
+        userRepository.save(user).block()
+
+        // Get all the users
+        val foundUser = webTestClient.get().uri("/api/users?sort=id,DESC")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isOk
+            .expectHeader().contentType(MediaType.APPLICATION_JSON)
+            .returnResult(UserDTO::class.java).responseBody.blockFirst()
+
+        assertThat(foundUser.login).isEqualTo(DEFAULT_LOGIN)
+    }
+
+    @Test
+    fun getAllAuthorities() {
+        webTestClient.get().uri("/api/authorities")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isOk
+            .expectHeader().contentType(MediaType.APPLICATION_JSON_VALUE)
+            .expectBody()
+            .jsonPath("$").isArray()
+            .jsonPath("$[?(@=='$ADMIN')]").hasJsonPath()
+            .jsonPath("$[?(@=='$USER')]").hasJsonPath()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun getAllUsersSortedByParameters() {
+        // Initialize the database
+        userRepository.save(user).block()
+
+        webTestClient.get().uri("/api/users?sort=resetKey,DESC").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isBadRequest
+        webTestClient.get().uri("/api/users?sort=password,DESC").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isBadRequest
+        webTestClient.get().uri("/api/users?sort=resetKey,id,DESC").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isBadRequest
+        webTestClient.get().uri("/api/users?sort=id,DESC").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk
+    }
+}
