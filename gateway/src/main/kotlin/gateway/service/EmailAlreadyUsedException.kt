121d8c7ef4599611efe2d7356316fc65f6dd9e24
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-02-08 15:02:00 +0100

    add projects to have a look

--- gateway/src/main/kotlin/gateway/service/EmailAlreadyUsedException.kt
+++
@@ -1,7 +0,0 @@
-package gateway.service
-
-class EmailAlreadyUsedException : RuntimeException("Email is already in use!") {
-    companion object {
-        private const val serialVersionUID = 1L
-    }
-}
--- gateway/src/main/kotlin/gateway/service/UsernameAlreadyUsedException.kt
+++
@@ -1,7 +0,0 @@
-package gateway.service
-
-class UsernameAlreadyUsedException : RuntimeException("Login name already used!") {
-    companion object {
-        private const val serialVersionUID = 1L
-    }
-}
