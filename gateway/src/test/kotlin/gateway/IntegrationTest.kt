e064e207ea6ca7f4c080e02ed48bc092878efad8
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-02-08 13:47:09 +0100

    Initial version of gateway generated by generator-jhipster@7.5.0 with blueprints: generator-jhipster-kotlin@1.14.0

---
+++ gateway/src/test/kotlin/gateway/IntegrationTest.kt
@@ -0,0 +1,13 @@
+package gateway
+
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@kotlin.annotation.Target(AnnotationTarget.CLASS)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+@SpringBootTest(classes = [GatewayApp::class])
+@ExtendWith(ReactiveSqlTestContainerExtension::class)
+annotation class IntegrationTest
