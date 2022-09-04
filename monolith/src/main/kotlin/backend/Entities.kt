28b81c651e3ddc7f682720a22ddd5a8e49f88d7d
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-09-04 19:13:43 +0200

    cleaning, renaming and refactoring

--- monolith/src/main/kotlin/backend/Entities.kt
+++ monolith/src/main/kotlin/backend/Entities.kt
@@ -218,14 +218,14 @@ data class EmailEntity(
 
 @Table("`authority`")
 data class AuthorityEntity(
-        @Id
+    @Id
     @field:NotNull
     @field:Size(max = 50)
     override val role: String
 ) : IAuthorityModel
 
-interface IAuthorityModel: Persistable<String> {
-val role:String
+interface IAuthorityModel : Persistable<String> {
+    val role: String
     override fun getId() = role
     override fun isNew() = true
 }
