63681e0e97c271aa9a36ced9c94f02fcb7798bbd
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-09-07 18:16:05 +0200

    adding AccountRepositoryR2dbcTest.kt to test AccountRepositoryR2dbc

--- monolith/src/test/kotlin/backend/AccountRepositoryR2dbcTest.kt
+++ monolith/src/test/kotlin/backend/AccountRepositoryR2dbcTest.kt
@@ -1,35 +1,38 @@
-@file:Suppress("NonAsciiCharacters")
+@file:Suppress("NonAsciiCharacters", "unused")
 
 package backend
 
+import backend.tdd.testLoader
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.springframework.beans.factory.getBean
+import org.springframework.boot.runApplication
+import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
 import org.springframework.stereotype.Repository
+import kotlin.test.Test
 
 @Repository
 abstract class AccountRepositoryR2dbc(
-    private val repository: R2dbcEntityTemplate
+    private val repository: R2dbcEntityTemplate,
+    private val authorityRepository: AuthorityRepository
 ) : AccountRepository
 
 internal class AccountRepositoryR2dbcTest {
-//    private lateinit var context: ConfigurableApplicationContext
-//    private val authorityRepository: AuthorityRepositoryR2dbc by lazy { context.getBean() }
-//
-//    @BeforeAll
-//    fun `lance le server en profile test`() =
-//        runApplication<Server> { testLoader(app = this) }
-//            .run { context = this }
-//
-//    @AfterAll
-//    fun `arrête le serveur`() = context.close()
-//
-//    @Test
-//    fun findOne(): Unit = runBlocking {
-//        mapOf(
-//            ROLE_ADMIN to ROLE_ADMIN,
-//            ROLE_USER to ROLE_USER,
-//            ROLE_ANONYMOUS to ROLE_ANONYMOUS,
-//            "" to null,
-//            "foo" to null
-//        ).map { assertEquals(it.value, authorityRepository.findOne(it.key)) }
-//    }
+    private lateinit var context: ConfigurableApplicationContext
+
+    private val accountRepository: AccountRepository by lazy { context.getBean<AccountRepositoryR2dbc>() }
+
+    @BeforeAll
+    fun `lance le server en profile test`() =
+        runApplication<Server> { testLoader(app = this) }
+            .run { context = this }
+
+    @AfterAll
+    fun `arrête le serveur`() = context.close()
+
+    @Test
+    fun findOneByLogin(): Unit = runBlocking {
+    }
 }
