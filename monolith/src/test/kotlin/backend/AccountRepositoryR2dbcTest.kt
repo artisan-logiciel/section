e665ea31aba6d6aa269ef6dd211963145715ac43
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-09-09 23:53:28 +0200

    implementing account r2dbc repositories.

--- monolith/src/test/kotlin/backend/AccountRepositoryR2dbcTest.kt
+++ monolith/src/test/kotlin/backend/AccountRepositoryR2dbcTest.kt
@@ -14,38 +14,36 @@ import org.junit.jupiter.api.BeforeAll
 import org.springframework.beans.factory.getBean
 import org.springframework.boot.runApplication
 import org.springframework.context.ConfigurableApplicationContext
-import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
-import org.springframework.data.r2dbc.core.awaitOneOrNull
-import org.springframework.data.r2dbc.core.delete
-import org.springframework.data.r2dbc.core.select
+import org.springframework.data.r2dbc.core.*
 import org.springframework.data.relational.core.query.Criteria.where
 import org.springframework.data.relational.core.query.Query.query
 import org.springframework.stereotype.Repository
 import reactor.kotlin.core.publisher.toMono
+import java.util.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import java.util.*
 
 @Repository
-class AccountAuthorityRepositoryR2dbc :AccountAuthorityRepository{
+class AccountAuthorityRepositoryR2dbc(
+    private val dao: R2dbcEntityTemplate,
+) : AccountAuthorityRepository {
     override suspend fun save(id: UUID, authority: String) {
-        TODO("Not yet implemented")
+        dao.insert(AccountAuthorityEntity(userId = id, role = authority)).awaitSingle()
     }
 
     override suspend fun delete(id: UUID, authority: String) {
-        TODO("Not yet implemented")
+        dao.delete(AccountAuthorityEntity(userId = id, role = authority)).awaitSingle()
     }
 
-    override suspend fun count(): Long {
-        TODO("Not yet implemented")
-    }
+    override suspend fun count(): Long = dao.select<AccountAuthorityEntity>().count().awaitSingle()
+
 
     override suspend fun deleteAll() {
-        TODO("Not yet implemented")
+        dao.delete<AccountAuthorityEntity>().allAndAwait()
     }
 
     override suspend fun deleteAllByAccountId(id: UUID) {
-        TODO("Not yet implemented")
+        dao.delete<AccountAuthorityEntity>().matching(query(where("userId").`is`(id))).allAndAwait()
     }
 
 }
@@ -53,41 +51,34 @@ class AccountAuthorityRepositoryR2dbc :AccountAuthorityRepository{
 
 @Repository
 class AccountRepositoryR2dbc(
-    private val repository: R2dbcEntityTemplate,
-    private val authorityRepository: AuthorityRepository
+    private val dao: R2dbcEntityTemplate, private val authorityRepository: AuthorityRepository
 ) : AccountRepository {
     override suspend fun save(model: AccountCredentials): Account? =
-        repository.insert(AccountEntity(model)).awaitSingleOrNull()?.toModel()
+        dao.insert(AccountEntity(model)).awaitSingleOrNull()?.toModel()
 
-    override suspend fun count(): Long =
-        repository.select<AccountEntity>().count().awaitSingle()
+    override suspend fun count(): Long = dao.select<AccountEntity>().count().awaitSingle()
 
     override suspend fun delete(account: Account) {
-        repository.delete(account).awaitSingleOrNull()
+        dao.delete(account).awaitSingleOrNull()
     }
 
     override suspend fun findOneByLogin(login: String): AccountCredentials? =
-        repository.select<AccountEntity>()
-            .matching(query(where("login").`is`(login)))
-            .awaitOneOrNull()?.toCredentialsModel()
+        dao.select<AccountEntity>().matching(query(where("login").`is`(login))).awaitOneOrNull()?.toCredentialsModel()
 
 
     override suspend fun findOneByEmail(email: String): AccountCredentials? =
-        repository.select<AccountEntity>()
-            .matching(query(where("email").`is`(email)))
-            .awaitOneOrNull()?.toCredentialsModel()
+        dao.select<AccountEntity>().matching(query(where("email").`is`(email))).awaitOneOrNull()?.toCredentialsModel()
 
     override suspend fun suppress(account: Account) {
-        repository.run {
-            delete<AccountAuthorityEntity>()
-                .matching(query(where("userId").`is`(account.id!!))).toMono().awaitSingle()
+        dao.run {
+            delete<AccountAuthorityEntity>().matching(query(where("userId").`is`(account.id!!))).toMono().awaitSingle()
             delete<AccountEntity>().toMono().awaitSingle()
         }
     }
 
 
     override suspend fun signup(model: AccountCredentials) {
-        repository.run {
+        dao.run {
             AccountEntity(model).run {
                 val id = insert(this).toMono().awaitSingleOrNull()?.id
                 if (id != null) authorities?.map {
@@ -99,13 +90,12 @@ class AccountRepositoryR2dbc(
     }
 
     override suspend fun findActivationKeyByLogin(login: String): String? =
-        repository.select<AccountEntity>().matching(query(where("login").`is`(login)))
-            .awaitOneOrNull()?.activationKey
+        dao.select<AccountEntity>().matching(query(where("login").`is`(login))).awaitOneOrNull()?.activationKey
 
 
     override suspend fun findOneActivationKey(key: String): AccountCredentials? =
-        repository.select<AccountEntity>().matching(query(where("activationKey").`is`(key)))
-            .awaitOneOrNull()?.toCredentialsModel()
+        dao.select<AccountEntity>().matching(query(where("activationKey").`is`(key))).awaitOneOrNull()
+            ?.toCredentialsModel()
 }
 
 internal fun createAccounts(accounts: Set<AccountCredentials>, repository: R2dbcEntityTemplate) {
