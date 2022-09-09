c7d5cd34d7e4213ef74336dd91c6d8f4f1eae0d6
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-09-09 21:09:48 +0200

    implementing in account r2dbc repositories.

--- monolith/src/test/kotlin/backend/AccountRepositoryR2dbcTest.kt
+++ monolith/src/test/kotlin/backend/AccountRepositoryR2dbcTest.kt
@@ -4,6 +4,8 @@ package backend
 
 import backend.data.Data
 import backend.tdd.testLoader
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
 import kotlinx.coroutines.reactor.mono
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.AfterAll
@@ -13,25 +15,38 @@ import org.springframework.beans.factory.getBean
 import org.springframework.boot.runApplication
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
+import org.springframework.data.r2dbc.core.awaitOneOrNull
 import org.springframework.data.r2dbc.core.select
+import org.springframework.data.relational.core.query.Criteria
+import org.springframework.data.relational.core.query.Query
 import reactor.kotlin.core.publisher.toMono
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class AccountRepositoryR2dbc(
-    private val repository: R2dbcEntityTemplate, private val authorityRepository: AuthorityRepository
+    private val repository: R2dbcEntityTemplate,
+    private val authorityRepository: AuthorityRepository
 ) : AccountRepository {
-    override suspend fun save(model: AccountCredentials): Account? = null
+    override suspend fun save(model: AccountCredentials): Account? =
+        repository.insert(AccountEntity(model)).awaitSingleOrNull()?.toModel()
 
-    override suspend fun count(): Long = -1L
+    override suspend fun count(): Long =
+        repository.select<AccountEntity>().count().awaitSingle()
 
     override suspend fun delete(account: Account) {
-
+        repository.delete(account).awaitSingleOrNull()
     }
 
-    override suspend fun findOneByLogin(login: String): AccountCredentials? = null
+    override suspend fun findOneByLogin(login: String): AccountCredentials? =
+        repository.select(AccountEntity::class.java)
+            .matching(Query.query(Criteria.where("login").`is`(login)))
+            .awaitOneOrNull()?.toCredentialsModel()
 
-    override suspend fun findOneByEmail(email: String): AccountCredentials? = null
+
+    override suspend fun findOneByEmail(email: String): AccountCredentials? =
+        repository.select(AccountEntity::class.java)
+            .matching(Query.query(Criteria.where("email").`is`(email)))
+            .awaitOneOrNull()?.toCredentialsModel()
 
     override suspend fun suppress(account: Account) {
 
@@ -66,6 +81,7 @@ internal class AccountRepositoryR2dbcTest {
     private lateinit var context: ConfigurableApplicationContext
 
     private val repository: R2dbcEntityTemplate by lazy { context.getBean() }
+    private val accountRepository: AccountRepository by lazy { context.getBean<AccountRepositoryR2dbc>() }
 
     @BeforeAll
     fun `lance le server en profile test`() = runApplication<Server> { testLoader(this) }.run { context = this }
@@ -81,12 +97,13 @@ internal class AccountRepositoryR2dbcTest {
     @Test
     fun save() = runBlocking {
         assertEquals(0, repository.select<AccountEntity>().count().block())
-        repository.insert(AccountEntity(Data.defaultAccount)).block()
+        accountRepository.save(Data.defaultAccount)
         assertEquals(1, repository.select<AccountEntity>().count().block())
     }
 
     @Test
     fun count() {
+
     }
 
     @Test
