1c1f2747179a4797007faa2ceeb7373a581d3778
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-09-10 03:03:56 +0200

    cleaning, renaming and refactoring

--- monolith/src/test/kotlin/backend/TestsUtils.kt
+++ monolith/src/test/kotlin/backend/TestsUtils.kt
@@ -1,14 +1,9 @@
 package backend
 
-import kotlinx.coroutines.reactor.awaitSingle
-import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
-import org.springframework.data.r2dbc.core.allAndAwait
 import org.springframework.data.r2dbc.core.awaitOneOrNull
 import org.springframework.data.r2dbc.core.select
-import org.springframework.data.relational.core.query.Criteria
 import org.springframework.data.relational.core.query.Criteria.where
-import org.springframework.data.relational.core.query.Query
 import org.springframework.data.relational.core.query.Query.query
 import reactor.kotlin.core.publisher.toMono
 import java.util.*
@@ -25,31 +20,29 @@ fun deleteAccounts(repository: R2dbcEntityTemplate) {
     repository.delete(AccountEntity::class.java).toMono().block()
 }
 
-suspend fun saveAccount(model: AccountCredentials, dao: R2dbcEntityTemplate): Account? {
-   return dao.insert(AccountEntity(model)).awaitSingleOrNull()?.toModel()
+fun saveAccount(model: AccountCredentials, dao: R2dbcEntityTemplate): Account? =
+    dao.insert(AccountEntity(model)).block()?.toModel()
+
+fun saveAccountAuthority(id: UUID, role: String, dao: R2dbcEntityTemplate): AccountAuthorityEntity? =
+    dao.insert(AccountAuthorityEntity(userId = id, role = role)).block()
+
+fun countAccount(dao: R2dbcEntityTemplate): Int =
+    dao.select(AccountEntity::class.java).count().block()?.toInt()!!
+
+
+fun countAccountAuthority(dao: R2dbcEntityTemplate): Int =
+    dao.select(AccountAuthorityEntity::class.java).count().block()?.toInt()!!
+
+
+fun deleteAllAccountAuthority(dao: R2dbcEntityTemplate) {
+    dao.delete(AccountAuthorityEntity::class.java).toMono().block()
 }
 
-fun saveAccountAuthority(id: UUID, roleUser: String, dao: R2dbcEntityTemplate) {
 
-}
-
-suspend fun countAccount(dao: R2dbcEntityTemplate): Int =
-    dao.select(AccountEntity::class.java).count().awaitSingle().toInt()
-
-
-suspend fun countAccountAuthority(dao: R2dbcEntityTemplate): Int =
-    dao.select(AccountAuthorityEntity::class.java).count().awaitSingle().toInt()
-
-
-suspend fun deleteAllAccountAuthority(dao: R2dbcEntityTemplate) {
-    dao.delete(AccountAuthorityEntity::class.java).allAndAwait()
-}
-
-
-suspend fun findOneByLogin(login: String, dao: R2dbcEntityTemplate): AccountCredentials? {
+fun findOneByLogin(login: String, dao: R2dbcEntityTemplate): AccountCredentials? {
     return dao.select<AccountEntity>()
         .matching(query(where("login").`is`(login)))
-        .awaitOneOrNull()?.toCredentialsModel()
+        .one().block()?.toCredentialsModel()
 }
 
 suspend fun findOneByEmail(email: String, dao: R2dbcEntityTemplate): AccountCredentials? {
