4d78804f2fc4a3e07242b67239a75e6ca29212ea
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-09-11 03:02:35 +0200

    cleaning, renaming, refactoring & fixing tests.

--- monolith/src/test/kotlin/backend/AccountAuthorityRepositoryR2dbcTest.kt
+++ monolith/src/test/kotlin/backend/AccountAuthorityRepositoryR2dbcTest.kt
@@ -1,22 +1,46 @@
+@file:Suppress("NonAsciiCharacters")
+
 package backend
 
+import backend.tdd.testLoader
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.getBean
+import org.springframework.boot.runApplication
+import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
 
 internal class AccountAuthorityRepositoryR2dbcTest {
 
-    private lateinit var dao: R2dbcEntityTemplate
+    private lateinit var context: ConfigurableApplicationContext
+
+    private val dao: R2dbcEntityTemplate by lazy { context.getBean() }
+    private val accountAuthorityRepository: AccountAuthorityRepository by lazy { context.getBean<AccountAuthorityRepositoryR2dbc>() }
+
+    @BeforeAll
+    fun `lance le server en profile test`() = runApplication<Server> {
+        testLoader(this)
+    }.run { context = this }
+
+    @AfterAll
+    fun `arrête le serveur`() = context.close()
+
+
+    @AfterEach
+    fun tearDown() = deleteAllAccounts(dao)
 
     @Test
     fun test_save() {
     }
 
     @Test
-    fun test_delete() {
+    fun test_count() {
     }
 
     @Test
-    fun test_count() {
+    fun test_delete() {
     }
 
     @Test
