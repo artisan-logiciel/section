775336c887d6b53887d9f528bb11089f0af33ddb
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-09-14 23:46:32 +0200

    cleaning, renaming, refactoring & fixing tests.

--- monolith/src/main/kotlin/backend/Services.kt
+++ monolith/src/main/kotlin/backend/Services.kt
@@ -22,10 +22,32 @@ import java.time.Instant
 import java.util.Locale.forLanguageTag
 import javax.mail.MessagingException
 @Service
+@Transactional
 class ChangePasswordService {
     fun changePassword(currentPassword: String, newPassword: String) {
         TODO("Not yet implemented")
     }
+    //    @Transactional
+//    suspend fun changePassword(currentClearTextPassword: String, newPassword: String) {
+//        SecurityUtils.getCurrentUserLogin().apply {
+//            if (!isNullOrBlank()) {
+//                userRepository.findOneByLogin(this).apply {
+//                    if (this != null) {
+//                        if (!passwordEncoder.matches(
+//                                currentClearTextPassword,
+//                                password
+//                            )
+//                        ) throw InvalidPasswordException()
+//                        else saveUser(this.apply {
+//                            password = passwordEncoder.encode(newPassword)
+//                        }).run {
+//                            log.debug("Changed password for User: {}", this)
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
 
 }
 
